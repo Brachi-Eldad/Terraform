30ee6af4bf98c5ab907daee56c929b8571858b2b
Brachi-Eldad <39329330385@mby.co.il>
2026-02-21 20:49:53 +0200

    Update main.tf

--- learn-terraform-docker-container/main.tf
+++ learn-terraform-docker-container/main.tf
@@ -15,11 +15,12 @@ resource "docker_image" "nginx" {
 }
 
 resource "docker_container" "nginx" {
-  image = docker_image.nginx.image_id
-  name  = brachi
+  image = docker_image.nginx.latest
+  name  = "Brachi"
   ports {
     internal = 80
     external = 8080
   }
 }
+
 
