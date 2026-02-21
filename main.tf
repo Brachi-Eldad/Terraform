d6d8082e325ec8b9f65810751a1ab73d85156488
Brachi-Eldad <39329330385@mby.co.il>
2026-02-21 20:50:11 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
     docker = {
-      source  = "kreuzwerker/docker"
-      version = "2.7.2"
+      source = "kreuzwerker/docker"
+      version = "~> 3.0.1"
     }
   }
 }
@@ -16,10 +16,9 @@ resource "docker_image" "nginx" {
 
 resource "docker_container" "nginx" {
   image = docker_image.nginx.latest
-  name  = brachi
+  name  = "Brachi"
   ports {
     internal = 80
     external = 8080
   }
 }
-
